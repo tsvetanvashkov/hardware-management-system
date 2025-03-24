af5c775ef6a51f151051e626d5f2623bd5a2ada9
tsvetanvashkov <tsvetan.vashkov@gmail.com>
2025-03-24 23:26:39 +0200

    Add projec creation

--- src/main/java/com/company/hardware_management_system/project/repository/ProjectRepository.java
+++ src/main/java/com/company/hardware_management_system/project/repository/ProjectRepository.java
@@ -4,9 +4,11 @@ import com.company.hardware_management_system.project.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProjectRepository  extends JpaRepository<Project, UUID> {
 
+    Optional<Project> findByName(String name);
 }
--- src/main/java/com/company/hardware_management_system/web/dto/AddProjectRequest.java
+++ src/main/java/com/company/hardware_management_system/web/dto/AddProjectRequest.java
@@ -1,4 +1,11 @@
 package com.company.hardware_management_system.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class AddProjectRequest {
+
+    @NotBlank(message = "Project name can not be empty.")
+    private String name;
 }
